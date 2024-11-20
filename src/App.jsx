03d4eca4cebed1004da12bf9b71f5f6230dedbb8
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-11-20 20:44:26 +0530

    starting professional portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import HomePage from "./pages/HomePage.jsx";
+function App() {
+  return (
+    <>
+      <HomePage></HomePage>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = () => {
+  return <div className="bg-black h-screen w-screen">HomePage</div>;
+};
+
+export default HomePage;
